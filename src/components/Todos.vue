ada130d70847310efd2e455da28ccef026e702a8
jiajianhwa <jiajianhwa@gmail.com>
2025-10-31 16:00:34 +0800

    Test deploy

---
+++ src/components/Todos.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import '@/assets/main.css';
+
+
+</script>
+
+<template>
+  <main>
+    <h1>My todos</h1>
+    <div>
+      🥳 App successfully hosted. Try creating a new todo.
+      <br />
+    </div>
+  </main>
+</template>
